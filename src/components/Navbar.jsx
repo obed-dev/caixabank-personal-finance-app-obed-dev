390c2e41c4f500d931d6510eef784e8f8feda7b0
obedbaltodano23 <obedbaltodano23r@gmail.com>
2024-11-14 16:17:19 -0600

    new responsive changes navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { AppBar, Toolbar, IconButton, Drawer, Box, Button, Badge, Typography, useMediaQuery } from '@mui/material';
+import {
+    AppBar, Toolbar, IconButton, Drawer, Box, Button, Badge, Typography, useMediaQuery
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
@@ -12,9 +14,9 @@ import CaixaBankLogo from '../assets/caixabank-icon.png';
 const Navbar = ({ toggleTheme, isDarkMode }) => {
     const [drawerOpen, setDrawerOpen] = useState(false);
     const navigate = useNavigate();
-    const isMobile = useMediaQuery('(max-width:600px)'); 
+    const isMobile = useMediaQuery('(max-width:800px)');  // Trigger at 800px
     const auth = useStore(authStore);
-    
+
     const toggleDrawer = (open) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
             return;
@@ -30,104 +32,85 @@ const Navbar = ({ toggleTheme, isDarkMode }) => {
     return (
         <>
             <AppBar position="static">
-                <Toolbar>
-                    {isMobile && (
-                        <>
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', paddingX: 2 }}>
+                    {/* Logo and Burger Menu Icon */}
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        {isMobile && (
                             <IconButton edge="start" aria-label="menu" onClick={toggleDrawer(true)}>
                                 <MenuIcon />
                             </IconButton>
-                            <Box sx={{ display: 'flex', alignItems: 'center', marginRight: 2 }}>
-                                <img src={CaixaBankLogo} alt="CaixaBank Logo" style={{ width: 32, height: 32, marginRight: 8 }} /> 
-                                <Typography variant="h6">CaixaBank</Typography>
-                            </Box>
-                        </>
-                    )}
+                        )}
+                        <img src={CaixaBankLogo} alt="CaixaBank Logo" style={{ width: 32, height: 32, marginRight: 8 }} />
+                        <Typography variant="h6" noWrap>
+                            CaixaBank
+                        </Typography>
+                    </Box>
 
-                    {!isMobile && (
-                        <Box sx={{ display: 'flex', alignItems: 'center', marginRight: 2 }}>
-                            <img src={CaixaBankLogo} alt="CaixaBank Logo" style={{ width: 32, height: 32, marginRight: 8 }} /> 
-                            <Typography variant="h6">CaixaBank</Typography>
+                    {/* Inline Links for Non-Mobile View */}
+                    {!isMobile && auth.isAuthenticated && (
+                        <Box sx={{ display: 'flex', alignItems: 'center', gap: 3, flexGrow: 1, justifyContent: 'flex-end' }}>
+                            <Link to="/dashboard" style={{ textDecoration: 'none', color: 'inherit' }}>Home</Link>
+                            <Link to="/transactions" style={{ textDecoration: 'none', color: 'inherit' }}>Transactions</Link>
+                            <Link to="/analysis" style={{ textDecoration: 'none', color: 'inherit' }}>Analysis</Link>
+                            <Link to="/settings" style={{ textDecoration: 'none', color: 'inherit' }}>Settings</Link>
+                            <Link to="/support" style={{ textDecoration: 'none', color: 'inherit' }}>Support</Link>
                         </Box>
                     )}
 
-                    <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: isMobile ? 'center' : 'flex-start' }}>
-                    {auth.isAuthenticated ? (  // Mostrar solo si el usuario está autenticado
-        !isMobile && (
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end', flexGrow: 1  }}>
-                <Link to="/dashboard" style={{ textDecoration: 'none', color: 'inherit', marginRight: 16 }}>
-                    Home
-                </Link>
-                <Link to="/transactions" style={{ textDecoration: 'none', color: 'inherit', marginRight: 16 }}>
-                    Transactions
-                </Link>
-                <Link to="/analysis" style={{ textDecoration: 'none', color: 'inherit', marginRight: 16 }}>
-                    Analysis
-                </Link>
-                <Link to="/settings" style={{ textDecoration: 'none', color: 'inherit', marginRight: 16 }}>
-                    Settings
-                </Link>
-                <Link to="/support" style={{ textDecoration: 'none', color: 'inherit', marginRight: 16 }}>
-                    Support
-                </Link>
-            </Box>
-        )
-    ) : null} 
-                            
-                            {auth.isAuthenticated && (
-                       <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: 'auto' }}>
-                       <IconButton color="inherit" onClick={toggleTheme}>
-                          {isDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}
-                        </IconButton>
-                      <IconButton color="inherit">
-                         <Badge color="error" variant="dot">
-                          <NotificationsIcon />
-                        </Badge>
-                         </IconButton>
-                 <Button color="inherit" onClick={handleLogout} sx={{ marginLeft: 2 }}>
-                      Sign Out
-                   </Button>
-              </Box>
+                    {/* Theme Toggle, Notifications, and Sign Out */}
+                    {auth.isAuthenticated && (
+                        <Box sx={{ display: 'flex', alignItems: 'center'  }}>
+                            <IconButton color="inherit" onClick={toggleTheme}>
+                                {isDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+                            </IconButton>
+                            <IconButton color="inherit">
+                                <Badge color="error" variant="dot">
+                                    <NotificationsIcon />
+                                </Badge>
+                            </IconButton>
+                            <Button color="inherit" onClick={handleLogout} sx={{ marginLeft: 2 }}>
+                                Sign Out
+                            </Button>
+                        </Box>
                     )}
-
-                    </Box>
                 </Toolbar>
             </AppBar>
 
-            {/* Burger menu for mobile view */}
+            {/* Drawer for Mobile View */}
             <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
-    <Box role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)} sx={{ width: 250, backgroundColor: 'background.paper' }}>
-        <Box sx={{ padding: 2 }}>
-            {!auth.isAuthenticated ? (
-                <>
-                    <Link to="/login" style={{ display: 'block', textDecoration: 'none', margin: '8px 0', color: 'inherit' }}>
-                        <Typography sx={{ color: 'text.primary' }}>Login</Typography>
-                    </Link>
-                    <Link to="/register" style={{ display: 'block', textDecoration: 'none', margin: '8px 0', color: 'inherit' }}>
-                        <Typography sx={{ color: 'text.primary' }}>Register</Typography>
-                    </Link>
-                </>
-            ) : (
-                <>
-                    <Link to="/dashboard" style={{ display: 'block', textDecoration: 'none', margin: '8px 0', color: 'inherit' }}>
-                        <Typography sx={{ color: 'text.primary' }}>Home</Typography>
-                    </Link>
-                    <Link to="/transactions" style={{ display: 'block', textDecoration: 'none', margin: '8px 0', color: 'inherit' }}>
-                        <Typography sx={{ color: 'text.primary' }}>Transactions</Typography>
-                    </Link>
-                    <Link to="/analysis" style={{ display: 'block', textDecoration: 'none', margin: '8px 0', color: 'inherit' }}>
-                        <Typography sx={{ color: 'text.primary' }}>Analysis</Typography>
-                    </Link>
-                    <Link to="/settings" style={{ display: 'block', textDecoration: 'none', margin: '8px 0', color: 'inherit' }}>
-                        <Typography sx={{ color: 'text.primary' }}>Settings</Typography>
-                    </Link>
-                    <Link to="/support" style={{ display: 'block', textDecoration: 'none', margin: '8px 0', color: 'inherit' }}>
-                        <Typography sx={{ color: 'text.primary' }}>Support</Typography>
-                    </Link>
-                </>
-            )}
-        </Box>
-    </Box>
-</Drawer>
+                <Box role="presentation" onClick={toggleDrawer(false)} onKeyDown={toggleDrawer(false)} sx={{ width: 250, backgroundColor: 'background.paper' }}>
+                    <Box sx={{ padding: 2 }}>
+                        {auth.isAuthenticated ? (
+                            <>
+                                <Link to="/dashboard" style={{ display: 'block', textDecoration: 'none', margin: '20px 0', color: 'inherit' }}>
+                                    <Typography sx={{ color: 'text.primary' }}>Home</Typography>
+                                </Link>
+                                <Link to="/transactions" style={{ display: 'block', textDecoration: 'none', margin: '20px 0', color: 'inherit' }}>
+                                    <Typography sx={{ color: 'text.primary' }}>Transactions</Typography>
+                                </Link>
+                                <Link to="/analysis" style={{ display: 'block', textDecoration: 'none', margin: '20px 0', color: 'inherit' }}>
+                                    <Typography sx={{ color: 'text.primary' }}>Analysis</Typography>
+                                </Link>
+                                <Link to="/settings" style={{ display: 'block', textDecoration: 'none', margin: '20px 0', color: 'inherit' }}>
+                                    <Typography sx={{ color: 'text.primary' }}>Settings</Typography>
+                                </Link>
+                                <Link to="/support" style={{ display: 'block', textDecoration: 'none', margin: '20px 0', color: 'inherit' }}>
+                                    <Typography sx={{ color: 'text.primary' }}>Support</Typography>
+                                </Link>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/login" style={{ display: 'block', textDecoration: 'none', margin: '10px 0', color: 'inherit' }}>
+                                    <Typography sx={{ color: 'text.primary' }}>Login</Typography>
+                                </Link>
+                                <Link to="/register" style={{ display: 'block', textDecoration: 'none', margin: '10px 0', color: 'inherit' }}>
+                                    <Typography sx={{ color: 'text.primary' }}>Register</Typography>
+                                </Link>
+                            </>
+                        )}
+                    </Box>
+                </Box>
+            </Drawer>
         </>
     );
 };
